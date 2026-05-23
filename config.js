9e0f8c46bdd61e1f35fca562e2b526574a50e8cf
DominicDev <s0l0estudi4r@gmail.com>
2026-05-23 23:07:40 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'THIRTY SECONDS TO MARS';
+export let nameSongs = [
+	"WALK ON WATER", /*1*/
+	"THIS IS WAR", /*2*/
+	"HURRICANE", /*3*/
+	"NIGHT OF THE HUNTER", /*4*/
+	"SEARCH AND DESTROY", /*5*/
+	"STRANGER IN A STRANGE LAND", /*6*/
+	"VOX POPULI", /*7*/
+	"ALIBI", /*8*/
+	"CLOSER TO THE EDGE", /*9*/
+	"KINGS AND QUEENS", /*10*/
+	"FROM YESTERDAY", /*11*/
+	"THE KILL", /*12*/
+	"A BEAUTIFUL LIE", /*13*/
+	"THE STORY", /*14*/
+	"WAS IT A DREAM?", /*15*/
+	"ATTACK", /*16*/
+	"R-EVOLVE", /*17*/
+	"UP IN THE AIR", /*18*/
+	"CITY OF ANGELS", /*19*/
+	"RESCUE ME", /*20*/
+]
